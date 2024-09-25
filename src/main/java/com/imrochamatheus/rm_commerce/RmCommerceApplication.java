ea1b6191dce4173dd123581dd7a07038516f868c
Matheus Rocha <matheus.rocha@cittamobi.com.br>
2024-09-24 21:33:17 -0300

    :tada: feat: adicionar estrutura inicial do projeto

---
+++ src/main/java/com/imrochamatheus/rm_commerce/RmCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.imrochamatheus.rm_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imrochamatheus/rm_commerce/RmCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imrochamatheus.rm_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
